856010ac2c1d368eb4f13ea2e624bc094c7fa64a
Saliou LO <losaliou.sl33@gmail.com>
2024-03-28 01:31:55 +0000

    update clerk/webhook

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -4,10 +4,9 @@
 
 // Resource: https://docs.svix.com/receiving/verifying-payloads/why
 // It's a good practice to verify webhooks. Above article shows why we should do it
-import { Webhook, WebhookRequiredHeaders } from "svix";
+import { Webhook } from "svix";
 import { headers } from "next/headers";
-
-import { IncomingHttpHeaders } from "http";
+import { WebhookEvent } from "@clerk/nextjs/server";
 
 import { NextResponse } from "next/server";
 import {
@@ -20,46 +19,30 @@ import {
 
 // Resource: https://clerk.com/docs/integration/webhooks#supported-events
 // Above document lists the supported events
-type EventType =
-  | "organization.created"
-  | "organizationInvitation.created"
-  | "organizationMembership.created"
-  | "organizationMembership.deleted"
-  | "organization.updated"
-  | "organization.deleted";
-
-type Event = {
-  data: Record<string, string | number | Record<string, string>[]>;
-  object: "event";
-  type: EventType;
-};
 
 export const POST = async (request: Request) => {
-  const payload = await request.json();
+  const payloadString = await request.text();
   const header = headers();
 
   const heads = {
-    "svix-id": header.get("svix-id"),
-    "svix-timestamp": header.get("svix-timestamp"),
-    "svix-signature": header.get("svix-signature"),
+    "svix-id": header.get("svix-id") || "",
+    "svix-timestamp": header.get("svix-timestamp") || "",
+    "svix-signature": header.get("svix-signature") || "",
   };
 
   // Activitate Webhook in the Clerk Dashboard.
   // After adding the endpoint, you'll see the secret on the right side.
   const wh = new Webhook(process.env.NEXT_CLERK_WEBHOOK_SECRET || "");
 
-  let evnt: Event | null = null;
+  let evnt: WebhookEvent | null = null;
 
   try {
-    evnt = wh.verify(
-      JSON.stringify(payload),
-      heads as IncomingHttpHeaders & WebhookRequiredHeaders
-    ) as Event;
+    evnt = wh.verify(payloadString, heads) as WebhookEvent;
   } catch (err) {
     return NextResponse.json({ message: err }, { status: 400 });
   }
 
-  const eventType: EventType = evnt?.type!;
+  const eventType = evnt?.type;
 
   // Listen organization creation event
   if (eventType === "organization.created") {
@@ -74,7 +57,7 @@ export const POST = async (request: Request) => {
         // @ts-ignore
         id,
         name,
-        slug,
+        slug || "",
         logo_url || image_url,
         "org bio",
         created_by
